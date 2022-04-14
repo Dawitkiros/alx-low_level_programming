9320ba56fa33f1e21b07a5a23091da060c278196
dawitkiros <dawitkiros1922@gmail.com>
2022-04-14 00:02:46 -0700

    Creating main files

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h_
+#define main_h_
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif /* main_H */
