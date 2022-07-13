88ad8042c9526efd7c5837946f0151962cd75243
dawitkiros <dawitkiros1922@gmail.com>
2022-07-13 10:27:42 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.comAnteneh2121/alx-low_level_programming/main/0x18-dynamic_libraries/libinjection.so
-export LD_PRELOAD="$PWD/../libinjection.so"
+
+gcc *.o -shared -o libruin.so
+
+LD_PRELOAD=$PWD/libruin.so
